e19b8dfd3c7b2f449ba8354f0cad952a39f43ba4
Wiktoria Kuna <wkuna@antmicro.com>
2024-07-11 12:14:32 +0200

    genhtml: Format soruce code; remove typos
    Signed-off-by: Wiktoria Kuna <wkuna@antmicro.com>

--- .github/scripts/indexgen/genhtml.py
+++ .github/scripts/indexgen/genhtml.py
@@ -17,7 +17,7 @@ Generate HTML-based coverage reports from "lcov -l" summaries of
 info files.
 
 positional arguments:
-  input_files           Paths to summmary files
+  input_files           Paths to summary files
 
 options:
   -h, --help            show this help message and exit
@@ -26,12 +26,11 @@ options:
                         report/)
 """
 
-from collections import defaultdict, OrderedDict
-from copy import deepcopy
-from pathlib import Path
-import sys
 import argparse
 import os
+from collections import OrderedDict, defaultdict
+from copy import deepcopy
+from pathlib import Path
 
 REPORT_TEMPLATE = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
@@ -100,21 +99,23 @@ REPORT_TEMPLATE = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//
 </html>
 """
 
+
 def get_color(value: float, min_value: float = 0, max_value: float = 100):
-    midpoint = (max_value-min_value) / 2
+    midpoint = (max_value - min_value) / 2
     if value <= midpoint:
-        return (255, int(255*value/midpoint), 0)
-    return (int(255*(max_value-value)/midpoint), 255, 0)
+        return (255, int(255 * value / midpoint), 0)
+    return (int(255 * (max_value - value) / midpoint), 255, 0)
+
 
 def process_line(line, data, filename):
     segments = line.split("|")
-    data[segments[0].strip()][filename.split('/')[-1].split('_')[-1].split('.')[0]] = list(
+    data[segments[0].strip()][filename.split("/")[-1].split("_")[-1].split(".")[0]] = list(
         [i for i in segments[1].split(" ") if i != ""]
     )
 
-def generate_table(data, links = False):
-    return \
-        """
+
+def generate_table(data, links=False):
+    return """
 <table width="80%" cellpadding=1 cellspacing=1 border=0>
 <tr>
 <td width="40%">Directory</td>
@@ -126,19 +127,22 @@ def generate_table(data, links = False):
 <token1>
 
 </table>
-        """.replace("<token1>", generate_table_tokenstr(data, links))
+        """.replace(
+        "<token1>", generate_table_tokenstr(data, links)
+    )
 
-def generate_table_tokenstr(data, links = False):
+
+def generate_table_tokenstr(data, links=False):
     ddata = dict(data)
     token_str = ""
 
     num_tests = len(list(list(ddata.items())[0][1].items()))
     raw_widths = [40, 20, 20]
-    widths_arr = [str(i / num_tests)+"%" for i in raw_widths]
-    token_str += "<tr><td width=20% style=\"border-top: 0px; border-left: 0px;\"></td>"
+    widths_arr = [str(i / num_tests) + "%" for i in raw_widths]
+    token_str += '<tr><td width=20% style="border-top: 0px; border-left: 0px;"></td>'
     for key in list(list(ddata.items())[0][1].keys()):
-        token_str += "<td style=\"text-align: center;\" width="
-        token_str += str(sum(raw_widths) / num_tests)+"%"
+        token_str += '<td style="text-align: center;" width='
+        token_str += str(sum(raw_widths) / num_tests) + "%"
         token_str += " colspan = 3>"
         token_str += key[0].upper() + key[1:]
         token_str += "</td>"
@@ -152,7 +156,7 @@ def generate_table_tokenstr(data, links = False):
         token_str += "<td width=20%>"
         if links:
             token_str += "<a href=index_"
-            token_str += file.replace("/","_")
+            token_str += file.replace("/", "_")
             token_str += ".html>"
             token_str += file
             token_str += "</a>"
@@ -161,39 +165,40 @@ def generate_table_tokenstr(data, links = False):
         token_str += "</td>"
 
         for key, numbers in cov_data.items():
-            r,g,b = get_color(float(numbers[0].replace("%","")))
+            r, g, b = get_color(float(numbers[0].replace("%", "")))
             cov_color = "#%s%s%s;" % tuple([hex(c)[2:].rjust(2, "0") for c in (r, g, b)])
 
             token_str += "<td width="
             token_str += widths_arr[0]
             token_str += ">"
-            token_str += "<div class=\"container\">"
-            token_str += "<div style=\"border-radius: 15px; height: 80%; background-color: "
+            token_str += '<div class="container">'
+            token_str += '<div style="border-radius: 15px; height: 80%; background-color: '
             token_str += cov_color
             token_str += "width: "
-            if float(numbers[0].replace("%","")) > 5:
+            if float(numbers[0].replace("%", "")) > 5:
                 token_str += numbers[0]
             else:
                 token_str += "5%"
-            token_str += ";\">&nbsp</div></div></td>"
+            token_str += ';">&nbsp</div></div></td>'
 
             token_str += "<td width="
             token_str += widths_arr[1]
-            token_str += " style=\"text-align: center; color: "
+            token_str += ' style="text-align: center; color: '
             token_str += cov_color
-            token_str += ";\">"
+            token_str += ';">'
             token_str += numbers[0]
             token_str += "</td>"
 
             token_str += "<td width="
             token_str += widths_arr[2]
-            token_str += " style=\"text-align: center;\">"
-            token_str +=  str(int(int(numbers[1]) * (float(numbers[0].replace("%",""))/100)))
+            token_str += ' style="text-align: center;">'
+            token_str += str(int(int(numbers[1]) * (float(numbers[0].replace("%", "")) / 100)))
             token_str += " / "
             token_str += numbers[1]
             token_str += "</td>"
         token_str += "</tr>"
     return token_str
+
 
 SUMMARY_TEMPLATE = """
 <td class="headerItem"><token1></td>
@@ -204,28 +209,21 @@ SUMMARY_TEMPLATE = """
 <td class="headerCovTableEntry"><token5></td>
 """
 
+
 def format_key(key: str):
     return key[0].upper() + key[1:] + ":"
 
-def generate_summary(data: list, key: str, new_row = False):
-    r,g,b = get_color(float(data[0].strip("%")))
+
+def generate_summary(data: list, key: str, new_row=False):
+    r, g, b = get_color(float(data[0].strip("%")))
     full_cov_color = "#%s%s%s;" % tuple([hex(c)[2:].rjust(2, "0") for c in (r, g, b)])
 
-    inner_row = SUMMARY_TEMPLATE.replace(
-        "<token1>",
-        format_key(key)
-    ).replace(
-        "<token2>",
-        full_cov_color
-    ).replace(
-        "<token3>",
-        data[0]
-    ).replace(
-        "<token4>",
-        str(int(int(data[1]) * (float(data[0].replace("%",""))/100)))
-    ).replace(
-        "<token5>",
-        data[1]
+    inner_row = (
+        SUMMARY_TEMPLATE.replace("<token1>", format_key(key))
+        .replace("<token2>", full_cov_color)
+        .replace("<token3>", data[0])
+        .replace("<token4>", str(int(int(data[1]) * (float(data[0].replace("%", "")) / 100))))
+        .replace("<token5>", data[1])
     )
 
     if new_row:
@@ -233,18 +231,20 @@ def generate_summary(data: list, key: str, new_row = False):
 
     return inner_row
 
-def render_page(data, view, out_dir, links = False):
+
+def render_page(data, view, out_dir, links=False):
     report_html = deepcopy(REPORT_TEMPLATE)
     report_html = report_html.replace("<easily_replacable_token_1>", "Full")
     for it, test in enumerate(data["Total:"].keys()):
-        tok = "<test_tokenX>".replace("X", str(it+1))
-        report_html = report_html.replace(tok, generate_summary(data['Total:'][test], test))
+        tok = "<test_tokenX>".replace("X", str(it + 1))
+        report_html = report_html.replace(tok, generate_summary(data["Total:"][test], test))
     report_html = report_html.replace("<easily_replacable_token_6>", generate_table(data, links))
     report_html = report_html.replace("<easily_replacable_token_7>", view)
-    with open(out_dir, 'w') as f:
-        print(report_html, file = f)
+    with open(out_dir, "w") as f:
+        print(report_html, file=f)
 
-def generate_dir_dict(data,dir):
+
+def generate_dir_dict(data, dir):
     gdict = defaultdict(lambda: defaultdict(list))
     for file, cov_data in dict(data).items():
         if file == "Total:":
@@ -259,7 +259,8 @@ def generate_dir_dict(data,dir):
             gdict[str(base)][key].append(data)
     return OrderedDict(sorted(gdict.items()))
 
-def generate_file_dict(data,dir: Path):
+
+def generate_file_dict(data, dir: Path):
     gdict = defaultdict(lambda: defaultdict(list))
     hit, total = defaultdict(int), defaultdict(int)
     for file, cov_data in dict(data).items():
@@ -268,17 +269,20 @@ def generate_file_dict(data,dir: Path):
         base = Path("/".join(dir.parts[1:]))
         if Path(file).parent == base:
             for key, data in cov_data.items():
-                # gdict[str(base)][key].append(data)
                 gdict[Path(file).name][key] = data
                 frac = float(data[0].strip("%")) / 100
                 hit[key] += int(frac * int(data[1]))
                 total[key] += int(data[1])
         for key, data in cov_data.items():
             if total[key] > 0:
-                gdict["Total:"][key] = ["{:.1f}%".format(hit[key]/total[key]*100), str(total[key])]
+                gdict["Total:"][key] = [
+                    "{:.1f}%".format(hit[key] / total[key] * 100),
+                    str(total[key]),
+                ]
             else:
                 gdict["Total:"][key] = ["0%", "0"]
     return gdict
+
 
 def unify_dict(data):
     # Figure out the list of all tests:
@@ -293,11 +297,12 @@ def unify_dict(data):
                 cov_data[test] = ["0.0%", "0"]
     return data
 
+
 def main(input_files, output_dir):
     data = defaultdict(lambda: defaultdict(list))
     code_root_path = None
     for i in input_files:
-        with open(i, 'r') as f:
+        with open(i, "r") as f:
             for line in f:
                 if line.strip().startswith(("|", "=", "Filename", "Reading tracefile")):
                     continue
@@ -310,7 +315,7 @@ def main(input_files, output_dir):
 
     data = unify_dict(data)
     code_root_path = Path(str(code_root_path))
-    tld=generate_dir_dict(data, code_root_path)
+    tld = generate_dir_dict(data, code_root_path)
 
     for key in list(tld.keys()):
         if key == "Total:":
@@ -319,41 +324,39 @@ def main(input_files, output_dir):
         render_page(
             subdata,
             "<a href=index.html>top level</a> - " + " - ".join(key.split("/")),
-            f"{output_dir}/index_{key.replace('/','_')}.html"
+            f"{output_dir}/index_{key.replace('/','_')}.html",
         )
 
     for file, cov_data in tld.items():
         if file == "Total:":
             continue
         for test_type, dat in cov_data.items():
-            hit, total = 0,0
+            hit, total = 0, 0
             for measurement in dat:
                 frac = float(measurement[0].strip("%")) / 100
                 hit += int(frac * int(measurement[1]))
                 total += int(measurement[1])
             if total > 0:
-                cov_data[test_type] = ["{:.1f}%".format(hit/total*100), str(total)]
+                cov_data[test_type] = ["{:.1f}%".format(hit / total * 100), str(total)]
             else:
                 cov_data[test_type] = ["0%", "0"]
     render_page(tld, "top level", f"{output_dir}/index.html", True)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate HTML-based coverage reports from \"lcov -l\" summaries of info files."
+        description='Generate HTML-based coverage reports from "lcov -l" summaries of info files.'
     )
     parser.add_argument(
-        'summary_files',
-        metavar='input_files',
-        type=str,
-        nargs='+',
-        help='Paths to summmary files'
+        "summary_files", metavar="input_files", type=str, nargs="+", help="Paths to summary files"
     )
-    parser.add_argument('--output-dir',
-		metavar='output_directory',
-		type=str,
-		default='report',
-		help='Path to output directory (default: report/)'
-	)
+    parser.add_argument(
+        "--output-dir",
+        metavar="output_directory",
+        type=str,
+        default="report",
+        help="Path to output directory (default: report/)",
+    )
 
     args = parser.parse_args()
 
