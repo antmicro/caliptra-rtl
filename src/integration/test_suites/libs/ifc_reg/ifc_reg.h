e0f14dca3da9274fbe972eab687133a9a820ce36
Maciej Dudek <mdudek@antmicro.com>
2026-05-19 13:50:11 +0200

    Add xorshift32() function to ifc_reg
    Function was taken from caliptra-ss to replace rand() calls.
    Standard rand() implementation returns values from [0:2^30-1] range,
    which isn't good enough for some tests.
    xorshift32() returns value from the entire uint32_t range.
    
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- src/integration/test_suites/libs/ifc_reg/ifc_reg.h
+++ src/integration/test_suites/libs/ifc_reg/ifc_reg.h
@@ -27,6 +27,8 @@ extern ifc_reg_exp_dict_t g_expected_data_dict;
 uint32_t ifc_reg_read(uint32_t reg_addr);
 void ifc_reg_write(uint32_t reg_addr, uint32_t value);
 
+uint32_t xorshift32(void);
+
 const ifc_register_info_t* find_register_by_address(uint32_t address, ifc_register_group_t *group_index, int *reg_index, ifc_register_group_t start_index);
 int get_total_register_count(void);
 void init_reg_exp_dict(ifc_reg_exp_dict_t *dict);
