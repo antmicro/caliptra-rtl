59f2d6fef0bbc167aa2f741eba8af85a24365f06
Olivier Betschi <obetschi@antmicro.com>
2026-05-29 14:22:53 +0000

    First version of jtag parallel access test

---
+++ src/integration/test_suites/jtag_parallel_access/jtag_parallel_access.s
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "caliptra_defines.h"
+
+.set    mfdc, 0x7f9
+.set    mfdht, 0x7ce
+.set    mstatus, 0x300
+.set    mitcnt0, 0x7d2
+.set    mitb0, 0x7d3
+.set    mitctl0, 0x7d4
+.set    mie, 0x304
+.set    mpmc, 0x7c6
+
+// Code to execute
+.section .text
+.global _start
+_start:
+
+    // Enable Caches in MRAC
+    li      x1, 0xaaaaaaaa
+    csrw    0x7c0, x1
+
+    li      x3, 4
+    csrw    mfdc, x3        // disable store merging
+
+    // Set some register values
+    li      x1,  0x12345678
+    li      x2,  0xABCDEF00
+    li      x3,  0xCAFEBABA
+    li      x4,  0xDEADBEEF
+    li      x5,  0x05050505
+    li      x6,  0xA0A0A0A0
+    li      x7,  0x00FF00FF
+    li      x8,  0xCC00CC00
+    li      s1,  0xFEEDABED // Writing 0 to this register initiates CPU halt
+
+    // Simple infinite loop program with inner and outer loop
+    li      t3,  0
+outer:
+    addi    t3, t3, 1
+    li      t4, 63
+// Use the inner loop to generate random traffic
+inner:
+    beq     s1, zero, halt_cpu
+    addi    t4, t4, -1
+    li      s2, 0x30040000 // MBOX SRAM
+    li      s4, 0x10011054 // AES DATA_IN0 address
+    lw      s3, 0(s2)
+    lw      s3, 256(s2)
+    lw      s3, 512(s2)
+    lw      s3, 1024(s2)
+    sw      x1, 0(s4)
+    sw      x2, 4(s4)
+    sw      x3, 8(s4)
+    sw      x4, 12(s4)
+    bne     t4, zero, inner
+    j       outer
+
+halt_cpu:
+    // Set mit0 and halt core
+    li      t5, 0xf0
+    li      t6, 0x20000800
+    csrwi   mitcnt0, 0x00   // Internal timer 0 counter
+    csrw    mitb0, t5       // Internal timer 0 boundary
+    csrwi   mitctl0, 0x01   // Internal timer 0 enable
+    csrw    mie, t6         // Internal timer 0 local interrupt enable
+    csrwi   mstatus, 0x08   // Internal timer 0 global interrupt enable
+    csrwi   mpmc, 0x03      // Initiate core halt with disable on mit0 interrupt
+    li      s1, 0xFEEDABED  // Indicate halt init
+    j       outer           // Return to infinite loop
+
+.section .dccm
+.global stdout
+stdout: .word STDOUT
+.global verbosity_g
+verbosity_g: .word 2
+
+.global intr_count
+intr_count: .word 0
+.global cptra_intr_rcv
+cptra_intr_rcv: .word 0
---
+++ src/integration/test_suites/jtag_parallel_access/jtag_parallel_access.tcl
@@ -0,0 +1,61 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+init
+
+set script_dir [file dirname [info script]]
+source [file join $script_dir common.tcl]
+
+riscv set_mem_access sysbus progbuf abstract
+
+echo "Accessing AES IV register"
+set AES_IV_ADDR 0x10011044
+set data_sample [list 0xDEADBEEF 0x12345678 0xCAFEDECA 0x31415928]
+
+# Do repeated accessses to generate traffic
+proc repeat_access_and_check {} {
+    global AES_IV_ADDR
+    global data_sample
+    for {set counter 0} { $counter < 4 } {incr counter} {
+        echo "Loop counter : ${counter}"
+        for {set sample_index 0} { $sample_index < 4} { incr sample_index} {
+            set golden $data_sample
+            write_memory $AES_IV_ADDR 32 $golden phys
+            set actual [read_memory $AES_IV_ADDR 32 4 phys ]
+            if {[compare $actual $golden] != 0} {
+                shutdown error
+            }
+        }
+    }
+}
+
+repeat_access_and_check
+
+# Trigger a FW reset update
+set SOC_IFC_FW_UPDATE_RST_ADDR 0x30030624
+write_memory $SOC_IFC_FW_UPDATE_RST_ADDR 32 0x1 phys
+
+# Check value after reset, register is auto-clearing
+# Hence expecting 0x0 instead of previously written 0x1
+set golden 0x0
+set actual [read_memory $SOC_IFC_FW_UPDATE_RST_ADDR 32 1 phys ]
+if {[compare $actual $golden] != 0} {
+    shutdown error
+}
+
+# Restart traffic
+repeat_access_and_check
+
+# Success
+shutdown
